75fc96928ade424b64561117865234f2bf341e07
Tekka <tekka.gb@gmail.com>
2024-09-16 21:16:29 -0700

    Added Episode Aigis support

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set GAME="D:\SteamLibrary\steamapps\common\P3R\P3R\Binaries\Win64\P3R.exe"
 
 ::Compile bmd uassets for every localized language
 for /d %%i in (.\UnrealEssentials\P3R\Content\L10N\*) do (
-    %COMPILER% .\bmd\BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+    %COMPILER% .\bmd\Xrd777_BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
     if exist %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset (
         move /y %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset
         del /s /q %%i\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
@@ -15,9 +15,15 @@ for /d %%i in (.\UnrealEssentials\P3R\Content\L10N\*) do (
         move /y %%i\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility_new.uasset %%i\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility.uasset
         del /s /q %%i\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility_new
     )
+    ::Episode Aigis bmd uasset
+    %COMPILER% .\bmd\Astrea_BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+    if exist %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset (
+        move /y %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset
+        del /s /q %%i\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+    )
 )
 :: Compile Japanese bmd uassets
-%COMPILER% .\bmd\BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+%COMPILER% .\bmd\Xrd777_BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
 if exist .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset (
     move /y .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset
     del /s /q .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
@@ -27,6 +33,12 @@ if exist .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtilit
     move /y .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility_new.uasset .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility.uasset
     del /s /q .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BMD_FldUtility_new
 )
+::Episode Aigis Japanaese bmd uasset
+%COMPILER% .\bmd\Astrea_BMD_FldSupport_AutoRecover.msg -Compile -OutFormat V1RE -Library P3RE -Encoding UTF-8  -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+if exist .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset (
+    move /y .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new.uasset .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover.uasset
+    del /s /q .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BMD_FldSupport_AutoRecover_new
+)
 
 ::Compile bf uassets
 %COMPILER% .\bf\BF_FldUtility.flow -Compile -OutFormat V4BE -Library P3RE -Encoding UTF-8 -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BF_FldUtility.uasset -Out .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BF_FldUtility_new
@@ -34,7 +46,7 @@ if exist .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtilit
     move /y .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BF_FldUtility_new.uasset .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BF_FldUtility.uasset
     del /s /q .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\FldUtility\BF_FldUtility_new
 )
-%COMPILER% .\bf\BF_FldSupport_AutoRecover.flow -Compile -OutFormat V4BE -Library P3RE -Encoding UTF-8 -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new
+%COMPILER% .\bf\Xrd777_BF_FldSupport_AutoRecover.flow -Compile -OutFormat V4BE -Library P3RE -Encoding UTF-8 -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new
 if exist .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new.uasset (
     move /y .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new.uasset .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover.uasset
     del /s /q .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new
@@ -43,6 +55,12 @@ if exist .\UnrealEssentials\P3R\Content\Xrd777\Field\Data\DataAsset\Bf\Dungeon\S
 if exist .\UnrealEssentials\P3R\Content\Xrd777\Schedule\Data\DataAsset\Bf\BF_P3HDscheduler_04_new.uasset (
     move /y .\UnrealEssentials\P3R\Content\Xrd777\Schedule\Data\DataAsset\Bf\BF_P3HDscheduler_04_new.uasset .\UnrealEssentials\P3R\Content\Xrd777\Schedule\Data\DataAsset\Bf\BF_P3HDscheduler_04.uasset
     del /s /q .\UnrealEssentials\P3R\Content\Xrd777\Schedule\Data\DataAsset\Bf\BF_P3HDscheduler_04_new
+)
+::Episode Aigis bf uasset
+%COMPILER% .\bf\Astrea_BF_FldSupport_AutoRecover.flow -Compile -OutFormat V4BE -Library P3RE -Encoding UTF-8 -Hook -SumBits -UPatch .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover.uasset -Out .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new
+if exist .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new.uasset (
+    move /y .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new.uasset .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover.uasset
+    del /s /q .\UnrealEssentials\P3R\Content\Astrea\Field\Data\DataAsset\Bf\Dungeon\SupportTalk\BF_FldSupport_AutoRecover_new
 )
 
 del /s /q .\AtlusScriptCompiler.log
